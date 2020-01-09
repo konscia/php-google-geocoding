40cfffc8cd61997433c8e2119a1f6af731707ba3
Kaléu Caminha <kaleu.caminha@gmail.com>
2020-01-09 19:02:30 -0300

    Aperfeiçoamento do teste de geolocalização com filtro por componentes e recuperando somente os dados estritamente necessários.

--- tests/test_geocoding_api.php
+++ tests/test_geocoding_api.php
@@ -8,8 +8,14 @@ $config = require __DIR__ . '/../config.php';
 $climate = new CLImate();
 
 $address = urlencode('Rua Pastor William, 1200');
+$components = [
+    'country' => 'BR',
+    'administrative_area' => 'Florianópolis, SC',
+    'postal_code' => '88034100'
+];
+$componentsUrl = urlencode(str_replace('=', ':', http_build_query($components, null, '|')));
 $key = $config['GOOGLE-API-KEY'];
-$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key={$key}";
+$url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&components={$componentsUrl}&key={$key}";
 $content = file_get_contents($url);
 $obj = json_decode($content, true);
 
@@ -17,8 +23,22 @@ $status = $obj['status'];
 switch ($status) {
     case 'OK':
         $climate->green('Sucesso');
-        $results = $obj['results'];
-        $climate->dump($results);
+        $result = $obj['results'][0];
+
+        $geometry = $result['geometry'];
+        $location = $geometry['location'];
+
+        $relevantData = [
+            'type' => $result['types'][0],
+            'place_id' => $result['place_id'],
+            'partial_match' => $result['partial_match'] ?? false,
+            'location_type' => $geometry['location_type'],
+            'formatted_address' => $result['formatted_address'],
+            'lat' => $location['lat'],
+            'lng' => $location['lng'],
+        ];
+
+        $climate->dump($relevantData);
         break;
     case 'ZERO_RESULTS':
         $climate->info('Nenhum resultado encontrado');
